70d6e5e83ae0b0095b1f577c32c4706e4ddc9a19
Vardges Mirzakhanyan <74791598+Vardges-01@users.noreply.github.com>
2025-01-23 07:19:34 +0000

    fix client sendMessage

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -3,41 +3,41 @@ import fs from "fs";
 import FormData from "form-data";
 
 export class BotClient {
-    client: AxiosInstance;
+  client: AxiosInstance;
 
-    constructor(token: string) {
-        this.client = axios.create({
-            baseURL: "https://yoai.yophone.com/api/pub",
-            headers: {
-                post: {
-                    "X-YoAI-API-Key": token,
-                },
-            },
-        } as CreateAxiosDefaults);
+  constructor(token: string) {
+    this.client = axios.create({
+      baseURL: "https://yoai.yophone.com/api/pub",
+      headers: {
+        post: {
+          "X-YoAI-API-Key": token,
+        },
+      },
+    } as CreateAxiosDefaults);
+  }
+
+  async sendMessage(chatId: string, text: string): Promise<void> {
+    await this.client.post("/sendMessage", { to: chatId, text });
+  }
+
+  async sendPhoto(chatId: string, text: string, photo: string): Promise<void> {
+    if (!fs.existsSync(photo)) {
+      throw "File is not found at path " + photo;
     }
 
-    async sendMessage(chatId: string, text: string): Promise<void> {
-        await this.client.post("/sendMessage", { chatId, text });
-    }
+    const formdata = new FormData();
+    formdata.append("to", chatId);
+    formdata.append("text", text);
+    formdata.append("file", fs.createReadStream(photo), photo);
 
-    async sendPhoto(chatId: string, text: string, photo: string): Promise<void> {
-        if(!fs.existsSync(photo)){
-            throw "File is not found at path " + photo
-        }
+    await this.client.post("/sendMessage", formdata, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
 
-        const formdata = new FormData();
-        formdata.append("to", chatId);
-        formdata.append("text", text);
-        formdata.append("file", fs.createReadStream(photo), photo);
-
-        await this.client.post("/sendMessage", formdata, {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-        });
-    }
-
-    getUpdates(): Promise<any> {
-        return this.client.post("/getUpdates");
-    }
+  getUpdates(): Promise<any> {
+    return this.client.post("/getUpdates");
+  }
 }
